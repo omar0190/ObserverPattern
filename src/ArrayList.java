895c850586632ab9554e5d82e43672c4c05ca2bb
Omar0190 <omar0190@stud.kea.dk>
2020-03-06 09:45:18 +0100

    Initial commit

---
+++ src/ArrayList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList <String> f = new ArrayList<>();
+        f.add("go");
+        f.add("lo");
+
+    }
+}
---
+++ src/TrainingArrayList.java
@@ -0,0 +1,33 @@
+// En statisk metode tilhører klassen, og ikke et objekt.
+// Du skal instatiere d
+
+import java.util.ArrayList;
+
+public class TrainingArrayList {
+    public static void main(String[] args) {
+    ArrayList<String> arr = new ArrayList<String>();
+    arr.add("boo");
+    arr.add("baz");
+    arr.add("foo");
+
+        String str;
+        System.out.println(arrayListReturn(arr, str: "boo"));
+    }
+
+    public static boolean arrayListReturn(ArrayList list, String str){
+        if(str.equals(null)){
+            return false;
+        }
+
+
+        if (list.contains(str)){
+            return false;
+        }
+        else{
+            list.add(str);
+            return true;
+        }
+
+        return true;
+    }
+}
